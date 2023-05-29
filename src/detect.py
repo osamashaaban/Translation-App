b623332dfaddd980a2a295054aa89584a0edcc94
osama shaban <63675685+osamashaaban@users.noreply.github.com>
2023-05-29 03:58:38 +0300

    Update detect.py

--- src/detect.py
+++ src/detect.py
@@ -8,35 +8,25 @@ from keras.utils import pad_sequences
 import numpy as np
 import pickle
 
-
+# Loading Tokeninzer
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
-
-
+    
 max_length = 5
+
+# Loading Model
 custom_objects = {'CategoricalCrossentropy': CategoricalCrossentropy}
 model = load_model('language_classification.h5' ,custom_objects=custom_objects)
 
 predicted = None
+
 app = FastAPI()
-
-# sequences = tokenizer.texts_to_sequences(["data osa jdid text"])
-# test_data = pad_sequences(sequences, maxlen=max_length) 
-# # Make predictions on the new data using the loaded model
-# predictions = model.predict(test_data)
-# print(np.argmax(predictions))
-# if np.argmax(predictions) == 0:
-#     predicted = "Arabic"
-# elif np.argmax(predictions) == 1:
-#     predicted = "English"
-
-# print(predicted)
-
+# Define the input schema for the API
 class data(BaseModel):
     text:str
      
     
-
+# Define the API 
 @app.post("/text")
 async def main(data:data):
     sequences = tokenizer.texts_to_sequences([str(data.text)])
